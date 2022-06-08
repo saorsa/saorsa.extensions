be855c5d1b492e4cad87fa56e9ad965978d2377d
Atanas Dragolov <adragolov@gmail.com>
2022-06-08 17:32:39 +0300

    Adding type extensions

---
+++ src/Saorsa.Extensions/TypeUtilityExtensions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+
+namespace Saorsa;
+
+public static class TypeUtilityExtensions
+{
+    public static bool IsStruct(this Type type)
+    {
+        return type.IsValueType && !type.IsEnum && !type.IsPrimitive;
+    }
+    
+    public static bool IsGenericEnumeration(this Type type)
+    {
+        return type.IsGenericType && typeof(IEnumerable).IsAssignableFrom(type);
+    }
+    
+    public static bool IsSingleElementEnumeration(this Type type)
+    {
+        return
+            // Arrays
+            type.IsArray 
+            && type.GetArrayRank() == 1
+            && !type.GetElementType()!.IsArray 
+            // Generics
+            || type.IsGenericEnumeration()
+            && type.GenericTypeArguments.Length == 1;
+    }
+}
---
+++ tests/Saorsa.Extensions.Tests/TypeUtilityExtensions.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Saorsa.Extensions.Tests;
+
+public class TypeUtilityExtensions
+{
+    private static readonly Type[] SimpleTypes =
+    {
+        typeof(short),
+        typeof(int),
+        typeof(long),
+        typeof(ushort),
+        typeof(uint),
+        typeof(ulong),
+        typeof(char),
+        typeof(byte),
+        typeof(sbyte),
+        typeof(string),
+    };
+
+    private static readonly Type[] StructureTypes =
+    {
+        typeof(DateTime),
+        typeof(DateTimeOffset),
+        typeof(TimeSpan),
+        typeof(TimeOnly),
+        typeof(decimal),
+    };
+
+    private static readonly Type[] GenericEnumerationTypes =
+    {
+        typeof(List<object>),
+        typeof(List<string>),
+        typeof(List<byte>),
+        typeof(Dictionary<object, object>),
+        typeof(Dictionary<object, int>),
+        typeof(HashSet<string>),
+    };
+    
+    private static readonly Type[] NonGenericEnumerationTypes =
+    {
+        typeof(object[]),
+        typeof(string),
+        typeof(byte[])
+    };
+
+    private static readonly Type[] SingleElementEnumerationTypes =
+    {
+        typeof(int[]),
+        typeof(List<string>),
+        typeof(HashSet<string>),
+    };
+
+    private static readonly Type[] NonSingleElementEnumerationTypes =
+    {
+        typeof(int[][][][]),
+        typeof(int[][]),
+        typeof(int[,]),
+        typeof(Dictionary<string, string>),
+        typeof(string)
+    };
+    
+    private class TestDummyClass
+    {
+    }
+
+    private struct TestDummyStruct
+    {
+    }
+
+    private enum TestDummyEnum
+    {
+    }
+
+    [Test]
+    public void TestCompositeTypes()
+    {
+        Assert.False(typeof(TestDummyClass).IsStruct());
+        Assert.False(typeof(TestDummyEnum).IsStruct());
+        Assert.True(typeof(TestDummyStruct).IsStruct());
+    }
+    
+    [Test]
+    public void TestStructureTypes()
+    {
+        foreach (var st in StructureTypes)
+        {
+            Assert.True(st.IsStruct(),
+                $"Type '{st.Name}' is expected to be a structure.");
+        }
+    }
+
+    [Test]
+    public void TestNonStructureTypes()
+    {
+        foreach (var st in SimpleTypes)
+        {
+            Assert.False(st.IsStruct(),
+                $"Type '{st.Name}' is NOT a structure.");
+        }
+    }
+
+    [Test]
+    public void TestIsGenericEnumeration()
+    {
+        GenericEnumerationTypes.ToList().ForEach(t =>
+        {
+            Assert.True(t.IsGenericEnumeration(),
+                $"Type '{t}' is expected to be generic enumeration.");
+        });
+        
+        NonGenericEnumerationTypes.ToList().ForEach(t =>
+        {
+            Assert.False(t.IsGenericEnumeration(),
+                $"Type '{t}' is NOT a generic enumeration.");
+        });
+    }
+    
+    [Test]
+    public void TestEnumerationTypes()
+    {
+        SingleElementEnumerationTypes.ToList().ForEach(t =>
+        {
+            Assert.True(t.IsSingleElementEnumeration(),
+                $"Type '{t}' is expected to be single element enumeration.");
+        });
+        
+        NonSingleElementEnumerationTypes.ToList().ForEach(t =>
+        {
+            Assert.False(t.IsSingleElementEnumeration(),
+                $"Type '{t}' is NOT a single element enumeration.");
+        });
+    }
+}
